0d8b5723e04400f1bf2cd371179f198d1041b159
Bayu Aji Firmansyah <22128941+bayooow@users.noreply.github.com>
2018-07-14 06:40:42 +0700

    Add files via upload

---
+++ Data Structures/Tree/AVLTree.java
@@ -0,0 +1,194 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tree;
+
+import static java.lang.Math.max;
+
+/**
+ *
+ * @author Bayu Aji Firmansyah
+ */
+public class AVLTree {
+
+    class Node {
+
+        int data;
+        Node left;
+        Node right;
+        int height;
+
+        public Node(int data) {
+            this.data = data;
+            height = 1;
+        }
+    }
+
+    public int getBalance(Node n) {
+        if (n != null) {
+            return (getHeight(n.left) - getHeight(n.right));
+        }
+        return 0;
+    }
+
+    public int getHeight(Node n) {
+        if (n != null) {
+            return n.height;
+        }
+        return 0;
+    }
+
+    public Node rightRotate(Node y) {
+        Node x = y.left;
+        Node T2 = x.right;
+        x.right = y;
+        y.left = T2;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+        return x;
+    }
+
+    public Node leftRotate(Node x) {
+        Node y = x.right;
+        Node T2 = y.left;
+        y.left = x;
+        x.right = T2;
+        x.height = Math.max(getHeight(x.left), getHeight(x.right)) + 1;
+        y.height = Math.max(getHeight(y.left), getHeight(y.right)) + 1;
+        return y;
+    }
+
+    public Node insert(Node node, int data) {
+        if (node == null) {
+            return (new Node(data));
+        }
+        if (node.data > data) {
+            node.left = insert(node.left, data);
+        } else {
+            node.right = insert(node.right, data);
+        }
+        node.height = Math.max(getHeight(node.left), getHeight(node.right)) + 1;
+        int balance = getBalance(node);
+        if (balance > 1 && data < node.left.data) {
+            return rightRotate(node);
+        }
+        if (balance < -1 && data > node.right.data) {
+            return leftRotate(node);
+        }
+        if (balance > 1 && data > node.left.data) {
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
+        }
+        if (balance < -1 && data < node.right.data) {
+            node.right = rightRotate(node.right);
+            return leftRotate(node);
+        }
+        return node;
+    }
+
+    public Node smallestValueNode(Node node) {
+        Node current = node;
+        while (current.left != null) {
+            current = current.left;
+        }
+        return current;
+    }
+
+    Node deleteNode(Node root, int data) {
+        if (root == null) {
+            return root;
+        }
+        if (data < root.data) {
+            root.left = deleteNode(root.left, data);
+        } else if (data > root.data) {
+            root.right = deleteNode(root.right, data);
+        } else {
+            if ((root.left == null) || (root.right == null)) {
+                Node temp = null;
+                if (temp == root.left) {
+                    temp = root.right;
+                } else {
+                    temp = root.left;
+                }
+                if (temp == null) {
+                    temp = root;
+                    root = null;
+                } else {
+                    root = temp;
+                }
+            } else {
+                Node temp = smallestValueNode(root.right);
+                root.data = temp.data;
+                root.right = deleteNode(root.right, temp.data);
+            }
+        }
+        if (root == null) {
+            return root;
+        }
+        root.height = max(root.left.height, root.right.height + 1);
+        int balance = getBalance(root);
+        if (balance > 1 && getBalance(root.left) >= 0) {
+            return rightRotate(root);
+        }
+        if (balance > 1 && getBalance(root.left) < 0) {
+            root.left = leftRotate(root.left);
+            return rightRotate(root);
+        }
+        if (balance < -1 && getBalance(root.right) <= 0) {
+            return leftRotate(root);
+        }
+        if (balance < -1 && getBalance(root.right) > 0) {
+            root.right = rightRotate(root.right);
+            return leftRotate(root);
+        }
+        return root;
+    }
+
+    public void printInorder(Node root) {
+        if (root != null) {
+            printInorder(root.left);
+            System.out.print(" " + root.data);
+            printInorder(root.right);
+        }
+    }
+
+    public void printPostorder(Node root) {
+        if (root != null) {
+            printPostorder(root.left);
+            printPostorder(root.right);
+            System.out.print(" " + root.data);
+        }
+    }
+
+    public void printPreorder(Node root) {
+        if (root != null) {
+            System.out.print(" " + root.data);
+            printPreorder(root.left);
+            printPreorder(root.right);
+        }
+    }
+
+    public static void main(String args[]) {
+
+        Node root = null;
+        AVLTree i = new AVLTree();
+        root = i.insert(root, 5);
+        root = i.insert(root, 2);
+        root = i.insert(root, 3);
+        root = i.insert(root, 5);
+        root = i.insert(root, 7);
+        root = i.insert(root, 1);
+        root = i.insert(root, 4);
+        root = i.insert(root, 23);
+        root = i.insert(root, 0);
+        root = i.insert(root, 12);
+        i.printInorder(root);
+        System.out.println("");
+        i.printPostorder(root);
+        System.out.println("");
+        i.printPreorder(root);
+    }
+
+}
